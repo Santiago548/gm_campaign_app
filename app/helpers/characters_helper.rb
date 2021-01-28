fe8387634d093cb625ff476ef60a134b5329a150
Santiago548 <dominicbarajas548@gmail.com>
2021-01-28 15:01:15 -0700

    added index for languages and proficiencies

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,2 +1,107 @@
 module CharactersHelper
+    def character_language
+        ["Common",
+        "Dwarvish",
+        "Elvish",
+        "Gnomish",
+        "Goblin",
+        "Halfling",
+        "Orc"]
+    end
+
+    def character_weapons
+        ["Simple Weapons",
+        "Martial Weapons",
+        "Firearms Weapons",
+        "Exotic Weapons",]
+    end
+
+    def character_class
+        ["Barbarian",
+         "Bard",
+         "Cleric",
+         "Druid",
+         "Fighter",
+         "Monk",
+         "Paladin",
+         "Ranger",
+         "Rogue",
+         "Sorcerer",
+         "Warlock",
+         "Wizard",
+         "Artificer"]
+    end
+
+    def character_armor
+        ["Unarmored",
+         "Light-Armor",
+         "Medium-Armor",
+         "Heavy-Armor"]
+    end
+
+    def character_race
+        ["Dragonborn",
+         "Dwarf",
+         "Elf",
+         "Gnome",
+         "Half-Elf",
+         "Halfling",
+         "Half-Orc",
+         "Human",
+         "Tiefling",
+         "Orc of Exandria",
+         "Leonin",
+         "Satyr",
+         "Genasi",
+         "Goliath",
+         "Aasimar",
+         "Bugbear",
+         "Firbolg",
+         "Goblin",
+         "Hobgoblin",
+         "Kenku",
+         "Kobold",
+         "Lizardfolk",
+         "Orc",
+         "Tabaxi",
+         "Triton",
+         "Yuan-ti Pureblood",
+         "Feral Tiefling",
+         "Tortle",
+         "Changeling",
+         "Kalashtar",
+         "Orc of Eberron",
+         "Shifter",
+         "Warforged",
+         "Gith",
+         "Centaur",
+         "Loxodon",
+         "Minotaur",
+         "Simic hybrid",
+         "Vedalken",
+         "Verdan",
+         "Locathah",
+         "Grung"]
+    end
+
+    def character_skills
+        ['Athletics',
+         'Acrobatics',
+         'Sleight of Hand',
+         'Stealth',
+         'Arcana',
+         'History',
+         'Investigation',
+         'Nature',
+         'Religion',
+         'Animal Handling',
+         'Insight',
+         'Medicine',
+         'Perception',
+         'Survival',
+         'Deception',
+         'Intimidation',
+         'Performance',
+         'Persuasion']
+    end
 end
