65acb91059485c353ac42fdcb0d5dcd94c8b4e51
Santiago548 <dominicbarajas548@gmail.com>
2021-01-30 11:46:00 -0700

    omniauth method complete for signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,20 @@
 class SessionsController < ApplicationController
  
   def omniauth
-    user = User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
-      u.username = auth['info']['first_name']
-      u.email = auth['info']['email']
-      u.first_name = auth['info']['first_name']
-      u.last_name = auth['info']['last_name']
-      u.password = SecureRandom.hex(12)
-    end
+      user = User.create_from_omniauth(auth)
     if user.save
-      session[:user_id] = user.id
-      redirect_to new_character_path
+        session[:user_id] = user.id
+        redirect_to new_character_path
     else
-      flash[:message] = user.errors.full_messages.join(', ')
-       redirect_to characters_path
+        flash[:message] = user.errors.full_messages.join(', ')
+        redirect_to characters_path
     end
   end
 
   private
 
   def auth
-    request.env['omniauth.auth']
+      request.env['omniauth.auth']
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
     has_secure_password
     has_many :characters
     has_many :campaigns, through: :characters
+
+    def self.create_from_omniauth(auth)
+        User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
+            u.username = auth['info']['first_name']
+            u.email = auth['info']['email']
+            u.first_name = auth['info']['first_name']
+            u.last_name = auth['info']['last_name']
+            u.password = SecureRandom.hex(12)
+        end
+    end
 end
