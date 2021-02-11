2e7092886af595e30c2e821108bc34fe3f9144f1
Santiago548 <dominicbarajas548@gmail.com>
2021-02-10 18:20:45 -0700

    fixing player and game master index to show correct title

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+def display_user_header
+    @users.find_all { |obj| obj == true}
+    if @users == false
+        tag.fieldset(tag.h2("PLAYERS:"))
+    else
+        tag.fieldset(tag.h2("GAME MASTERS:"))
+    end
+end
 end
