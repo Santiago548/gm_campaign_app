09d0a4e99f7d022ae5dc4efdec5604b4fd4e3dee
Santiago548 <dominicbarajas548@gmail.com>
2021-02-12 17:13:07 -0700

    added scope methods for characters to be ordered by level, and campaigns to be ordered alphabetically.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,5 +2,5 @@ class Campaign < ApplicationRecord
     has_many :characters
     has_many :users, through: :characters
 
-    # scope :campaign_order_alphabet, -> { where(game_master: true)}
+    scope :campaign_order_alphabet, -> { order(name: :asc)}
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -3,4 +3,5 @@ class Character < ApplicationRecord
     belongs_to :user
     
     validates :user_id, :campaign_id, presence: true
+    scope :character_order_alphabet, -> { order(level: :asc)}
 end
